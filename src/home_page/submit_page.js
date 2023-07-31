4a95d55b9272fd1722ee1125a5c8d7dfc22f57b0
Quaglihaus <martinoquaglia@gmail.com>
2023-07-31 19:47:23 +0200

    people adjusted

--- src/home_page/submit_page.js
+++ src/home_page/submit_page.js
@@ -11,9 +11,9 @@ export function SubmitPage() {
         {guestName: 'Fabio', guestSurname: 'Stivala', guestAllergies: ''},
         {guestName: 'Valerio', guestSurname: 'Lisci', guestAllergies: ''},
         {guestName: 'Paolo', guestSurname: 'Bader', guestAllergies: ''},
-        {guestName: 'Laura', guestSurname: 'Sbraci', guestAllergies: ''}, //TODO
+        {guestName: 'Laura', guestSurname: 'Sbraci', guestAllergies: ''},
         {guestName: 'Matteo', guestSurname: 'Turchetti', guestAllergies: ''},
-        {guestName: 'Emanuela', guestSurname: 'Turchetti', guestAllergies: ''}, //TODO
+        {guestName: 'Emanuela', guestSurname: 'Piazza', guestAllergies: ''},
         {guestName: 'Gabriele', guestSurname: 'Salvo', guestAllergies: ''},
         {guestName: 'Bruno', guestSurname: 'Pinci', guestAllergies: ''},
         {guestName: 'Bianca', guestSurname: 'Pop', guestAllergies: ''},
@@ -25,14 +25,14 @@ export function SubmitPage() {
         {guestName: 'Martina', guestSurname: 'Pirali', guestAllergies: ''},
         {guestName: 'Lorenzo', guestSurname: 'Scialdone', guestAllergies: ''},
         {guestName: 'Filippo', guestSurname: 'Di Capita', guestAllergies: ''},
-        {guestName: 'Annalisa', guestSurname: 'Zallio', guestAllergies: ''},
+        {guestName: 'Annalisa', guestSurname: 'Zallio', guestAllergies: 'incintissima'},
         {guestName: 'Francesca', guestSurname: 'Sapey', guestAllergies: ''},
-        {guestName: 'Barbara', guestSurname: 'Sapey', guestAllergies: ''}, //TODO
+        {guestName: 'Barbara', guestSurname: 'Caponotto', guestAllergies: ''},
         {guestName: 'Federico', guestSurname: 'Sapey', guestAllergies: ''},
         {guestName: 'Mariachiara', guestSurname: 'Cataldo', guestAllergies: ''},
         {guestName: 'Piero', guestSurname: 'Cataldo', guestAllergies: ''},
-        {guestName: 'Consuelo', guestSurname: 'Cataldo', guestAllergies: ''}, //TODO
-        {guestName: 'Giulia', guestSurname: 'Chinigò', guestAllergies: ''},
+        {guestName: 'Consuelo', guestSurname: 'Lanziere', guestAllergies: ''},
+        {/*guestName: 'Giulia', guestSurname: 'Chinigò', guestAllergies: ''*/},
         {guestName: 'Noemi', guestSurname: 'Di Tondo', guestAllergies: ''},
         {guestName: 'Alessandro', guestSurname: 'Pisani', guestAllergies: ''},
         {guestName: 'Olimpia', guestSurname: 'Madonia', guestAllergies: ''},
@@ -45,7 +45,7 @@ export function SubmitPage() {
         {guestName: 'Massimo', guestSurname: 'Scazzola', guestAllergies: ''},
         {guestName: 'Davide', guestSurname: 'Mollo', guestAllergies: ''},
         {guestName: 'Ilaria', guestSurname: 'Carnino', guestAllergies: ''},
-        {guestName: 'Cristina', guestSurname: '', guestAllergies: ''},
+        {guestName: 'Cristina', guestSurname: 'Santangelo', guestAllergies: ''},
         {guestName: 'Davide', guestSurname: 'Tricarico', guestAllergies: ''},
         {guestName: 'Rachele', guestSurname: 'Tricarico', guestAllergies: ''},
         {guestName: 'Gianluca', guestSurname: 'Tricarico', guestAllergies: ''},
@@ -55,7 +55,7 @@ export function SubmitPage() {
         {guestName: 'Alice', guestSurname: 'Sairu', guestAllergies: ''},
         {guestName: 'Andrea', guestSurname: 'Carbone', guestAllergies: ''},
         {guestName: 'Cecilia', guestSurname: 'Tomatis', guestAllergies: ''},
-        {guestName: 'Giacomo', guestSurname: 'Tomatis', guestAllergies: ''}, //TODO
+        {guestName: 'Giacomo', guestSurname: 'Gerbotto', guestAllergies: ''},
         {guestName: 'Idil', guestSurname: 'Aralp', guestAllergies: ''},
         {guestName: 'Emma', guestSurname: 'Aralp', guestAllergies: ''},
         {guestName: 'Mina', guestSurname: 'Aralp', guestAllergies: ''},
@@ -80,33 +80,33 @@ export function SubmitPage() {
         {guestName: 'Umberto', guestSurname: 'Manca', guestAllergies: ''},
         {guestName: 'Silvia', guestSurname: 'Monticelli', guestAllergies: ''},
         {guestName: 'Vittorio', guestSurname: 'Baritello', guestAllergies: ''},
-        {guestName: 'Roberto', guestSurname: 'Baritello', guestAllergies: ''}, //TODO
+        {guestName: 'Roberto', guestSurname: 'Baritello', guestAllergies: ''},
         {guestName: 'Claudia', guestSurname: 'Baritello', guestAllergies: ''},
         {guestName: 'Maria', guestSurname: 'Betto', guestAllergies: ''},
         {guestName: 'Enrico', guestSurname: 'Betto', guestAllergies: ''},
-        {guestName: 'Laura', guestSurname: 'Betto', guestAllergies: 'incinta'}, //TODO
+        {guestName: 'Laura', guestSurname: 'Pellegrinelli', guestAllergies: 'incinta'},
         {guestName: 'Giulia', guestSurname: 'Betto', guestAllergies: ''},
-        {guestName: 'Marco', guestSurname: 'Betto', guestAllergies: ''}, //TODO
-        {guestName: 'Anna', guestSurname: 'Betto', guestAllergies: ''}, //TODO
-        {guestName: 'Chiara', guestSurname: 'Betto', guestAllergies: ''}, //TODO
-        {guestName: 'Francesco', guestSurname: 'Betto', guestAllergies: ''}, //TODO
+        {guestName: 'Marco', guestSurname: 'Caradonna', guestAllergies: ''},
+        {guestName: 'Anna', guestSurname: 'Caradonna', guestAllergies: ''},
+        {guestName: 'Chiara', guestSurname: 'Caradonna', guestAllergies: ''},
+        {guestName: 'Francesco', guestSurname: 'Caradonna', guestAllergies: ''},
         {guestName: 'Giovanni', guestSurname: 'Betto', guestAllergies: ''},
         {guestName: 'Anna', guestSurname: 'Monticelli', guestAllergies: ''},
         {guestName: 'Claudio', guestSurname: 'Betto', guestAllergies: ''},
         {guestName: 'Edoardo', guestSurname: 'Monticelli', guestAllergies: ''},
-        {guestName: 'Grazia', guestSurname: 'Monticelli', guestAllergies: ''}, //TODO
+        {guestName: 'Maria Grazia', guestSurname: 'Monticelli', guestAllergies: ''},
         {guestName: 'Andrea', guestSurname: 'Monticelli', guestAllergies: ''},
         {guestName: 'Regina', guestSurname: 'Monticelli', guestAllergies: ''}, //TODO
         {guestName: 'Paolo', guestSurname: 'Monticelli', guestAllergies: ''},
-        {guestName: 'Chiara', guestSurname: 'Monticelli', guestAllergies: ''}, //TODO
+        {guestName: 'Chiara', guestSurname: 'Adami', guestAllergies: ''},
         {guestName: 'Leonardo', guestSurname: 'Monticelli', guestAllergies: ''},
         {guestName: 'Alessandro', guestSurname: 'Monticelli', guestAllergies: ''},
-        {guestName: 'Cristina', guestSurname: '', guestAllergies: ''}, //TODO
-        {guestName: 'Franco', guestSurname: '', guestAllergies: ''}, //TODO
+        {guestName: 'Cristina', guestSurname: 'Monticelli', guestAllergies: ''},
+        {guestName: 'Franco', guestSurname: 'Monticelli', guestAllergies: ''},
         {guestName: 'Pier Paolo', guestSurname: 'don', guestAllergies: ''}, //TODO
         {guestName: 'Renato', guestSurname: 'don', guestAllergies: ''}, //TODO
         {guestName: 'Giuseppe', guestSurname: 'Quaglia', guestAllergies: ''},
-        {guestName: 'Liliana', guestSurname: '', guestAllergies: ''}, //TODO
+        {guestName: 'Liliana', guestSurname: 'Quaglia', guestAllergies: ''},
         {guestName: 'Annamaria', guestSurname: 'Quaglia', guestAllergies: ''},
         {guestName: 'Franco', guestSurname: 'Cavallone', guestAllergies: ''},
         {guestName: 'Nicolò', guestSurname: 'Cavallone', guestAllergies: ''},
@@ -121,16 +121,16 @@ export function SubmitPage() {
         {guestName: 'Lucia', guestSurname: 'Serra', guestAllergies: ''},
         {guestName: 'Paolo', guestSurname: 'Venturini', guestAllergies: ''},
         {guestName: 'Teresa', guestSurname: 'Serra', guestAllergies: ''},
-        {guestName: 'Francesco', guestSurname: 'Serra', guestAllergies: ''}, //TODO
+        {guestName: 'Francesco', guestSurname: 'Sturaro', guestAllergies: ''},
         {guestName: 'Alberto', guestSurname: 'Mazzucchelli', guestAllergies: ''},
         {guestName: 'Chiara', guestSurname: 'Del Ben', guestAllergies: ''},
         {guestName: 'Giacomo', guestSurname: 'Mazzucchelli', guestAllergies: ''},
         {guestName: 'Pietro', guestSurname: 'Salanitro', guestAllergies: ''},
         {guestName: 'Silvia', guestSurname: 'Chiaraviglio', guestAllergies: ''},
         {guestName: 'Michele', guestSurname: 'Serra', guestAllergies: ''},
-        {guestName: 'Luca', guestSurname: 'Lojacono', guestAllergies: ''},
-        {guestName: 'Moglie', guestSurname: 'Lojacono', guestAllergies: ''}, //TODO
-        {guestName: 'Pierluigi', guestSurname: 'Pezzini', guestAllergies: ''},
+        {/*guestName: 'Luca', guestSurname: 'Lojacono', guestAllergies: ''*/},
+        {/*guestName: 'Moglie', guestSurname: 'Lojacono', guestAllergies: ''*/},
+        {/*guestName: 'Pierluigi', guestSurname: 'Pezzini', guestAllergies: ''*/},
         {guestName: 'Stefano', guestSurname: 'Serra', guestAllergies: ''},
         {guestName: 'Marta', guestSurname: 'Saccaggi', guestAllergies: ''},
         {guestName: 'Caterina', guestSurname: 'Serra', guestAllergies: ''},
@@ -142,9 +142,9 @@ export function SubmitPage() {
         {guestName: 'Ludivica', guestSurname: 'Unnia', guestAllergies: ''},
         {guestName: 'Andrea', guestSurname: 'Fiorentino', guestAllergies: ''},
         {guestName: 'Teresa', guestSurname: 'Guarnaccia', guestAllergies: ''},
-        {guestName: 'Daniele', guestSurname: '', guestAllergies: ''}, //TODO
-        {guestName: 'Francesco', guestSurname: '', guestAllergies: ''}, //TODO
-        {guestName: 'Giulia', guestSurname: '', guestAllergies: ''}, //TODO
+        {guestName: 'Daniele', guestSurname: 'Rossetti', guestAllergies: ''},
+        {guestName: 'Francesco', guestSurname: 'Beretta', guestAllergies: ''},
+        {guestName: 'Giulia', guestSurname: 'Rizzi', guestAllergies: ''},
         {guestName: 'Paola', guestSurname: 'Saccone', guestAllergies: ''},
         {guestName: 'Davide', guestSurname: 'Salatino', guestAllergies: ''},
         {guestName: 'Angelica', guestSurname: 'Cirigliano', guestAllergies: ''},
